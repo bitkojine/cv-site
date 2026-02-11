83999d8a0d82db7d7ba13893e9ee63d5c523e7b8
bitkojine <74838686+bitkojine@users.noreply.github.com>
2026-02-11 20:34:46 +0200

    Refine event reporting and add coverage

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="../.astro/types.d.ts" />
+
+type EventProps = Record<string, unknown>;
+type EventReporter = (eventName: string, props?: EventProps) => void;
+
+declare global {
+  interface Window {
+    dataLayer?: Array<Record<string, unknown>>;
+    cvReportEvent?: EventReporter;
+    cvTrack?: EventReporter;
+  }
+}
+
+export {};
